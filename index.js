2796325bd18780e60881ad0163e8f10c92634286
Manuel Entrena <manuel.entrena@gmail.com>
2021-06-02 10:16:23 +0200

    Modificamos Puerto

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ app.use(cors());
 //Express.json habilitar
 app.use(express.json({ extended: true }));
 //Puerto de la app
-const PORT = process.env.PORT || 5000;
+const port = process.env.port || 5000;
 //Importar nuestras rutas
 app.use("/api/usuarios", usuariosRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/proyectos", proyectosRouter);
 app.use("/api/tareas", tareasRouter);
 //Arrancar server
-app.listen(PORT, () => {
-  console.log(`El servidor esta arrancado en el puerto ${PORT}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log(`El servidor esta arrancado en el puerto ${port}`);
 });
